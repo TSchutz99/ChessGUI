d841ad614d5bbc6ebe5ab3edee9645f900a67f95
Faun <Faun.schuetz@gmail.com>
2020-11-09 10:21:31 +0000

    Committing - Project.

---
+++ src/ChessGUI.java
@@ -0,0 +1,13 @@
+/* ChessGUI.java
+ * By: Faun Schutz
+ * Start: 09/11/2020
+ * Finish:
+ */
+public class ChessGUI {
+    public BoardFrame boardFrame;
+    public static void main(String[] args){
+        ChessGUI gui = new ChessGUI();
+        gui.boardFrame =new boardFrame();
+        gui.boardFrame.setVisible(true);
+    }
+}
