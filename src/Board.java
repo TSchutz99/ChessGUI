a6a7c2812144fef0a86c722b6d687493d76701ac
Faun <Faun.schuetz@gmail.com>
2020-11-09 10:42:04 +0000

    Committing - Project.

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 /* BoardFrame.java
  * By: Faun Schutz
  * Start: 09/11/2020
  * Finish:
  */
-public class Board extends JComponent {
+public class Board extends JComponent{
+    public int turnCounter = 0;
+    private static Image NULL_IMAGE =new BufferedImage(10, 10, BufferedImage.TYPE_INT_ARGB);
+
+    private final int Square_Width = 65;
+    public ArrayList<Piece> White_Pieces;
+    public ArrayList<Piece> Black_Pieces;
 }
