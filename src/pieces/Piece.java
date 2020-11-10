7ba0861c5bfb1612c37bac8591b5f2cbaffcc246
Faun <Faun.schuetz@gmail.com>
2020-11-10 19:02:39 +0000

    Committing - Project. Must write loadImage(), DrawingImage() int the Board file. But before that must first write the Piece file, will make things easier.

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -22,7 +22,14 @@ public class Piece {
     public String getFile_path(){
         return file_path;
     }
-    public void setFile_path(String file_path) {
+    public void setFile_path(String file_path){
         this.file_path = file_path;
     }
+    public boolean isWhite(){
+        return is_white;
+    }
+    public boolean isBlack(){
+        return !is_white;
+    }
+
 }
