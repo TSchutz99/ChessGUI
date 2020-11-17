68638c728488dc73de79882cfd02eeabb1911c64
Faun <Faun.schuetz@gmail.com>
2020-11-17 18:19:53 +0000

    Committing - Project.

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -33,6 +33,7 @@ public class Bishop extends Piece{
         else if(this.getX() < destination_x && this.getY() > destination_y)
             direction = "north-east";
 
+        // For test proposes
         System.out.println(direction);
 
         if(direction.equals("north-west")){
